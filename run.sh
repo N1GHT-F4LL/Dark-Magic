5159ec76adf32fdcda18fdf83ae343ecf628d746
Dang Minh Tri <60804710+N1GHT-F4LL@users.noreply.github.com>
2023-09-13 12:47:52 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
-# Đường dẫn đến thư mục chứa tệp pyovpn trong /usr/local/openvpn_as/lib/python
+# Check if the OpenVPN Access Server service is running
+if systemctl is-active --quiet openvpnas; then
+    # Stop the OpenVPN Access Server service if it's running
+    systemctl stop openvpnas
+    echo "Stopped the OpenVPN Access Server service."
+fi
+
+# Path to the directory containing the pyovpn file in /usr/local/openvpn_as/lib/python
 ovpnas_dir="/usr/local/openvpn_as/lib/python"
 
-# Tìm đường dẫn đến tệp pyovpn-2.0-py trong ovpnas_dir
-pyovpn_path=$(find "$ovpnas_dir" -type f -name "*pyovpn-2.0-py*")
-
-# URL của tệp pyovpn trên GitHub
+# Find the path to the pyovpn-2.0-py file in the ovpnas_dir
+#pyovpn_path=$(find "$ovpnas_dir" -type f -name "*pyovpn-2.0-py*")
+pyovpn_path=$ovpnas_dir
+# URL of the pyovpn file on GitHub
 pyovpn_url="https://github.com/user/repository/raw/main/pyovpn-2.0-py3.10.egg"
 
-# Kiểm tra xem tệp pyovpn đã tồn tại trong thư mục đích hay chưa
+# Check if the pyovpn file exists in the destination directory
 if [ -e "$pyovpn_path" ]; then
-    echo "Tệp pyovpn đã tồn tại trong thư mục gốc."
-    # Tải về tệp pyovpn từ GitHub
-    echo "Đang tải về tệp pyovpn từ GitHub..."
+    echo "The pyovpn file already exists in the root directory."
+    # Download the pyovpn file from GitHub
+    echo "Downloading the pyovpn file from GitHub..."
     curl -LJO "$pyovpn_url" -o "$pyovpn_path"
-    echo "Đã tải về và thay thế tệp pyovpn."
-    sudo /usr/local/openvpn_as/bin/ovpn-init
+    echo "Downloaded and replaced the pyovpn file."
 else
-    echo "Tệp pyovpn không tồn tại."
+    echo "The pyovpn file does not exist."
 fi
+
+# Start the OpenVPN Access Server service again
+systemctl start openvpnas
+echo "Started the OpenVPN Access Server service again."
