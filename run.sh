c3444a84e21c755d05629a4f8feb827a1fa9a878
Dang Minh Tri <60804710+N1GHT-F4LL@users.noreply.github.com>
2023-09-13 12:34:38 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
 # Đường dẫn đến thư mục chứa tệp pyovpn trong /usr/local/openvpn_as/lib/python
-pyovpn_dir="/usr/local/openvpn_as/lib/python"
+ovpnas_dir="/usr/local/openvpn_as/lib/python"
+
+# Tìm đường dẫn đến tệp pyovpn-2.0-py trong ovpnas_dir
+pyovpn_path=$(find "$ovpnas_dir" -type f -name "*pyovpn-2.0-py*")
 
 # URL của tệp pyovpn trên GitHub
-github_url="https://github.com/user/repository/raw/main/pyovpn-2.0-py3.10.egg"
-
-# Thư mục đích để lưu tệp pyovpn
-destination_dir="/path/to/destination"
+pyovpn_url="https://github.com/user/repository/raw/main/pyovpn-2.0-py3.10.egg"
 
 # Kiểm tra xem tệp pyovpn đã tồn tại trong thư mục đích hay chưa
-if [ -e "$pyovpn_dir/pyovpn-2.0-py3.10.egg" ]; then
+if [ -e "$pyovpn_path" ]; then
     echo "Tệp pyovpn đã tồn tại trong thư mục gốc."
-else
     # Tải về tệp pyovpn từ GitHub
     echo "Đang tải về tệp pyovpn từ GitHub..."
-    curl -LJO "$github_url" -o "$destination_dir/pyovpn-2.0-py3.10.egg"
-    
-    # Di chuyển tệp tải về vào thư mục gốc (được giả định là /usr/local/openvpn_as/lib/python)
-    mv "$destination_dir/pyovpn-2.0-py3.10.egg" "$pyovpn_dir/"
-    
+    curl -LJO "$pyovpn_url" -o "$pyovpn_path"
     echo "Đã tải về và thay thế tệp pyovpn."
+else
+    echo "$pyovpn_path" not found
 fi
