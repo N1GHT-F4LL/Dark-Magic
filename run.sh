49cb5ab032fabd98331d8f6f9d5e7c431a0f7649
Dang Minh Tri <60804710+N1GHT-F4LL@users.noreply.github.com>
2023-09-13 12:26:12 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
-access_server_path="/usr/local/openvpn_as/"
+# Đường dẫn đến thư mục chứa tệp pyovpn trong /usr/local/openvpn_as/lib/python
+pyovpn_dir="/usr/local/openvpn_as/lib/python"
 
-if [ -d "$access_server_path" ]; then
-  echo "OpenVPN Access Server đã được cài đặt tại: $access_server_path"
+# URL của tệp pyovpn trên GitHub
+github_url="https://github.com/user/repository/raw/main/pyovpn-2.0-py3.10.egg"
+
+# Thư mục đích để lưu tệp pyovpn
+destination_dir="/path/to/destination"
+
+# Kiểm tra xem tệp pyovpn đã tồn tại trong thư mục đích hay chưa
+if [ -e "$pyovpn_dir/pyovpn-2.0-py3.10.egg" ]; then
+    echo "Tệp pyovpn đã tồn tại trong thư mục gốc."
 else
-  echo "OpenVPN Access Server không được cài đặt trên hệ thống này."
+    # Tải về tệp pyovpn từ GitHub
+    echo "Đang tải về tệp pyovpn từ GitHub..."
+    curl -LJO "$github_url" -o "$destination_dir/pyovpn-2.0-py3.10.egg"
+    
+    # Di chuyển tệp tải về vào thư mục gốc (được giả định là /usr/local/openvpn_as/lib/python)
+    mv "$destination_dir/pyovpn-2.0-py3.10.egg" "$pyovpn_dir/"
+    
+    echo "Đã tải về và thay thế tệp pyovpn."
 fi
